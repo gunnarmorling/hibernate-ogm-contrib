hibernate/hibernate-ogm

OGM-262 Add integration with CouchDB

hibernate-ogm-couchdb/src/main/java/org/hibernate/ogm/dialect/couchdb/designdocument/CouchDBDesignDocument.java
@@ -1,0 +1,115 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.dialect.couchdb.designdocument;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.hibernate.ogm.dialect.couchdb.resteasy.CouchDBDocument;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Creates a CouchDB Design Documents
+ *
+ * It's serialized to Json in order to create a CouchDB Design Documents
+ *
+ * A Design Document contains a set of View.
+ *
+ * Each View has a name, a Javascript map function and an optional JavaScript reduce function
+ *
+ * @author Andrea Boriero <dreborier@gmail.com/>
+ */
+public class CouchDBDesignDocument extends CouchDBDocument {
+
+	private Map<String, View> views = new HashMap<String, View>();
+
+	private final String language = "javascript";
+
+	public Map<String, View> getViews() {
+		return views;
+	}
+
+	public void setViews(Map<String, View> views) {
+		this.views = views;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * Adds View with bot map and reduce functions to the Document.
+	 *
+	 * @param viewName
+	 *            the name of the View
+	 * @param map
+	 *            a String representing the JavaScript map function code
+	 * @param reduce
+	 *            a String representing the JavaScript reduce function code
+	 */
+	public void addView(String viewName, String map, String reduce) {
+		View view = new View();
+		view.setMap( map );
+		view.setReduce( reduce );
+		addView( viewName, view );
+	}
+
+	/**
+	 * Adds View with only the map function to the Document.
+	 *
+	 * @param viewName
+	 *            the name of the View
+	 * @param map
+	 *            a String representing the JavaScript map function code
+	 */
+	public void addView(String viewName, String map) {
+		View view = new View();
+		view.setMap( map );
+		addView( viewName, view );
+	}
+
+	private void addView(String viewName, View view) {
+		views.put( viewName, view );
+	}
+
+	@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+	public class View {
+		private String map;
+		private String reduce;
+
+		public String getMap() {
+			return map;
+		}
+
+		public void setMap(String map) {
+			this.map = map;
+		}
+
+		public String getReduce() {
+			return reduce;
+		}
+
+		public void setReduce(String reduce) {
+			this.reduce = reduce;
+		}
+
+	}
+}

hibernate-ogm-couchdb/src/main/java/org/hibernate/ogm/dialect/couchdb/model/CouchDBTupleSnapshot.java
@@ -1,0 +1,68 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.dialect.couchdb.model;
+
+import org.hibernate.ogm.datastore.spi.TupleSnapshot;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Andrea Boriero <dreborier@gmail.com/>
+ */
+public class CouchDBTupleSnapshot implements TupleSnapshot {
+
+	private CouchDBTuple tuple;
+
+	public CouchDBTupleSnapshot() {
+		tuple = new CouchDBTuple();
+	}
+
+	public CouchDBTupleSnapshot(CouchDBTuple tuple) {
+		this.tuple = tuple;
+	}
+
+	@Override
+	public Object get(String column) {
+		return tuple.getColumnValue( getCoumnPosition( column ) );
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return tuple.isEmpty();
+	}
+
+	@Override
+	public Set<String> getColumnNames() {
+		if ( tuple.getColumnNames() != null ) {
+			return new HashSet<String>( Arrays.asList( tuple.getColumnNames() ) );
+		}
+		else {
+			return new HashSet<String>();
+		}
+	}
+
+	private int getCoumnPosition(String column) {
+		return tuple.getColumNamePosition( column );
+	}
+
+}

hibernate-ogm-couchdb/src/test/java/org/hibernate/ogm/datastore/couchdb/impl/CouchDBDatastoreTest.java
@@ -1,0 +1,203 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.datastore.couchdb.impl;
+
+import org.hibernate.HibernateException;
+import org.hibernate.ogm.dialect.couchdb.CouchDBDialectTest;
+import org.hibernate.ogm.dialect.couchdb.Environment;
+import org.hibernate.ogm.dialect.couchdb.resteasy.CouchDBDocument;
+import org.hibernate.ogm.dialect.couchdb.resteasy.CouchDBEntity;
+import org.hibernate.ogm.dialect.couchdb.util.DataBaseURL;
+import org.hibernate.ogm.grid.EntityKey;
+import org.hibernate.ogm.grid.EntityKeyMetadata;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.OptimisticLockException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrea Boriero <dreborier@gmail.com/>
+ */
+public class CouchDBDatastoreTest {
+
+	public static final int DEFAULT_PORT = 5984;
+	public static final String LOCALHOST = "localhost";
+	private CouchDBDatastore dataStore;
+	private Properties properties;
+	private boolean dataBaseDropped;
+
+	@Before
+	public void setUp() throws Exception {
+		setUpDatastore();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		dropDatabase();
+	}
+
+	private void dropDatabase() {
+		if ( dataStore != null && !dataBaseDropped ) {
+			dataStore.dropDatabase();
+			dataBaseDropped = true;
+		}
+	}
+
+	@Test(expected = OptimisticLockException.class)
+	public void testUpdatingADocumentWithWrongRevisionNumber() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		String firstVersion = createdDocument.getRevision();
+
+		createdDocument = dataStore.saveDocument( createdDocument );
+		createdDocument.setRevision( firstVersion );
+
+		dataStore.saveDocument( createdDocument );
+	}
+
+	@Test(expected = HibernateException.class)
+	public void testCreatingADocumentAfterDeletingDatabase() {
+		dropDatabase();
+
+		dataStore.saveDocument( createEntity() );
+	}
+
+	@Test(expected = HibernateException.class)
+	public void testCreatingInstanceWithWrongUrlUpdatingADocumentUsingAWrongDatabaseUrlConfiguration() throws Exception {
+		dataStore = null;
+		CouchDBDatastore.newInstance( getWrongDatabaseURL(), getUserName(), getPassword(), true );
+	}
+
+	@Test
+	public void testDeleteADocument() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		String createdDocumentId = createdDocument.getId();
+		dataStore.deleteDocument( createdDocument );
+
+		CouchDBEntity entity = dataStore.getEntity( createdDocumentId );
+
+		assertThat( entity, nullValue() );
+	}
+
+	@Test(expected = OptimisticLockException.class)
+	public void testDeleteADocumentWithWrongRevision() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		final String revisionBeforeUpdate = createdDocument.getRevision();
+
+		// saving the document will change its revision value
+		dataStore.saveDocument( createdDocument );
+		createdDocument.setRevision( revisionBeforeUpdate );
+
+		dataStore.deleteDocument( createdDocument );
+	}
+
+	@Test(expected = HibernateException.class)
+	public void testDeleteADocumentNotSavedOnTheDatabase() {
+		CouchDBDocument document = createEntity();
+		dataStore.deleteDocument( document );
+	}
+
+	@Test
+	public void testGetEntity() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		CouchDBEntity entity = dataStore.getEntity( createdDocument.getId() );
+		assertThat( entity, notNullValue() );
+	}
+
+	@Test
+	public void testGetEntityWithWoringIdReturnNullValue() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		CouchDBEntity entity = dataStore.getEntity( createdDocument.getId() + "_1" );
+		assertThat( entity, nullValue() );
+	}
+
+	private void setUpDatastore() throws IOException {
+		dataBaseDropped = false;
+		loadProperties();
+		dataStore = CouchDBDatastore.newInstance( getDatabaseURL(), getUserName(), getPassword(), true );
+	}
+
+	private DataBaseURL getDatabaseURL() throws MalformedURLException {
+		return new DataBaseURL( getDatabaseHost(), getDatabasePort(), getDatabaseName() );
+	}
+
+	private void loadProperties() throws IOException {
+		properties = new Properties();
+		properties.load( CouchDBDialectTest.class.getClassLoader().getResourceAsStream( "hibernate.properties" ) );
+	}
+
+	private String getPassword() {
+		return properties.getProperty( Environment.COUCHDB_PASSWORD );
+	}
+
+	private String getUserName() {
+		return properties.getProperty( Environment.COUCHDB_USERNAME );
+	}
+
+	private int getDatabasePort() {
+		final String port = properties.getProperty( Environment.COUCHDB_PORT );
+		if ( isValueProvided( port ) ) {
+			return Integer.valueOf( port );
+		}
+		else {
+			return DEFAULT_PORT;
+		}
+	}
+
+	private String getDatabaseName() {
+		return properties.getProperty( Environment.COUCHDB_DATABASE );
+	}
+
+	private String getDatabaseHost() {
+		final String host = properties.getProperty( Environment.COUCHDB_HOST );
+		if ( isValueProvided( host ) ) {
+			return host;
+		}
+		else {
+			return LOCALHOST;
+		}
+	}
+
+	private CouchDBEntity createEntity() {
+		return new CouchDBEntity( createEntityKey( "tableName", new String[] { "id", "name" }, new String[] { "1",
+				"Andrea" } ) );
+	}
+
+	private EntityKey createEntityKey(String tableName, String[] columnNames, Object[] values) {
+		return new EntityKey( new EntityKeyMetadata( tableName, columnNames ), values );
+	}
+
+	private DataBaseURL getWrongDatabaseURL() throws MalformedURLException {
+		return new DataBaseURL( "localhost", 1234, "no_existing" );
+	}
+
+	private boolean isValueProvided(String property) {
+		return property != null && !property.trim().equals( "" );
+	}
+
+}

hibernate-ogm-couchdb/src/test/java/org/hibernate/ogm/dialect/couchdb/util/DataBaseURLTest.java
@@ -1,0 +1,60 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.dialect.couchdb.util;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrea Boriero <dreborier@gmail.com/>
+ */
+public class DataBaseURLTest {
+
+	@Test
+	public void shouldReturnTheCorrectServerURL() throws Exception {
+		String expectedServerURL = "http://localhost:5984";
+
+		DataBaseURL dataBaseURL = new DataBaseURL( "localhost", 5984, "databasename" );
+
+		assertThat( dataBaseURL.getServerUrl(), is( expectedServerURL ) );
+	}
+
+	@Test
+	public void shouldReturnTheCorrectServerName() throws Exception {
+		String expectedName = "not_important";
+
+		DataBaseURL dataBaseURL = new DataBaseURL( "localhost", 5984, expectedName );
+
+		assertThat( dataBaseURL.getDataBaseName(), is( expectedName ) );
+	}
+
+	@Test
+	public void shouldReturnTheCorectURLStringRerpresentation() throws Exception {
+		String expectedString = "http://localhost:5984/databaseName";
+
+		DataBaseURL dataBaseURL = new DataBaseURL( "localhost", 5984, "databaseName" );
+
+		assertThat( dataBaseURL.toString(), is( expectedString ) );
+	}
+
+}

gunnarmorling/hibernate-ogm-contrib

src/main/java/org/hibernate/ogm/dialect/couchdb/designdocument/CouchDBDesignDocument.java
@@ -1,0 +1,115 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.dialect.couchdb.designdocument;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.hibernate.ogm.dialect.couchdb.json.CouchDBDocument;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Creates a CouchDB Design Document
+ *
+ * It's serialized to Json in order to create a CouchDB Design Documents
+ *
+ * A Design Document contains a set of View.
+ *
+ * Each View has a name, a Javascript map function and an optional JavaScript reduce function
+ *
+ * @author Andrea Boriero <dreborier@gmail.com/>
+ */
+public class CouchDBDesignDocument extends CouchDBDocument {
+
+	private Map<String, View> views = new HashMap<String, View>();
+
+	private final String language = "javascript";
+
+	public Map<String, View> getViews() {
+		return views;
+	}
+
+	public void setViews(Map<String, View> views) {
+		this.views = views;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * Adds View with bot map and reduce functions to the Document.
+	 *
+	 * @param viewName
+	 *            the name of the View
+	 * @param map
+	 *            a String representing the JavaScript map function code
+	 * @param reduce
+	 *            a String representing the JavaScript reduce function code
+	 */
+	public void addView(String viewName, String map, String reduce) {
+		View view = new View();
+		view.setMap( map );
+		view.setReduce( reduce );
+		addView( viewName, view );
+	}
+
+	/**
+	 * Adds View with only the map function to the Document.
+	 *
+	 * @param viewName
+	 *            the name of the View
+	 * @param map
+	 *            a String representing the JavaScript map function code
+	 */
+	public void addView(String viewName, String map) {
+		View view = new View();
+		view.setMap( map );
+		addView( viewName, view );
+	}
+
+	private void addView(String viewName, View view) {
+		views.put( viewName, view );
+	}
+
+	@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+	public class View {
+		private String map;
+		private String reduce;
+
+		public String getMap() {
+			return map;
+		}
+
+		public void setMap(String map) {
+			this.map = map;
+		}
+
+		public String getReduce() {
+			return reduce;
+		}
+
+		public void setReduce(String reduce) {
+			this.reduce = reduce;
+		}
+
+	}
+}

src/main/java/org/hibernate/ogm/dialect/couchdb/model/CouchDBTupleSnapshot.java
@@ -1,0 +1,68 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.dialect.couchdb.model;
+
+import org.hibernate.ogm.datastore.spi.TupleSnapshot;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Andrea Boriero <dreborier@gmail.com/>
+ */
+public class CouchDBTupleSnapshot implements TupleSnapshot {
+
+	private final CouchDBTuple tuple;
+
+	public CouchDBTupleSnapshot() {
+		tuple = new CouchDBTuple();
+	}
+
+	public CouchDBTupleSnapshot(CouchDBTuple tuple) {
+		this.tuple = tuple;
+	}
+
+	@Override
+	public Object get(String column) {
+		return tuple.getColumnValue( getColumnPosition( column ) );
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return tuple.isEmpty();
+	}
+
+	@Override
+	public Set<String> getColumnNames() {
+		if ( tuple.getColumnNames() != null ) {
+			return new HashSet<String>( Arrays.asList( tuple.getColumnNames() ) );
+		}
+		else {
+			return new HashSet<String>();
+		}
+	}
+
+	private int getColumnPosition(String column) {
+		return tuple.getColumNamePosition( column );
+	}
+
+}

src/test/java/org/hibernate/ogm/datastore/couchdb/impl/CouchDBDatastoreTest.java
@@ -1,0 +1,204 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.datastore.couchdb.impl;
+
+import org.hibernate.HibernateException;
+import org.hibernate.ogm.dialect.couchdb.CouchDBDialectTest;
+import org.hibernate.ogm.dialect.couchdb.Environment;
+import org.hibernate.ogm.dialect.couchdb.json.CouchDBDocument;
+import org.hibernate.ogm.dialect.couchdb.json.CouchDBEntity;
+import org.hibernate.ogm.dialect.couchdb.util.DataBaseURL;
+import org.hibernate.ogm.grid.EntityKey;
+import org.hibernate.ogm.grid.EntityKeyMetadata;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.OptimisticLockException;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrea Boriero <dreborier@gmail.com/>
+ */
+public class CouchDBDatastoreTest {
+
+	public static final int DEFAULT_PORT = 5984;
+	public static final String LOCALHOST = "localhost";
+	private CouchDBDatastore dataStore;
+	private Properties properties;
+	private boolean dataBaseDropped;
+
+	@Before
+	public void setUp() throws Exception {
+		setUpDatastore();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		dropDatabase();
+	}
+
+	private void dropDatabase() {
+		if ( dataStore != null && !dataBaseDropped ) {
+			dataStore.dropDatabase();
+			dataBaseDropped = true;
+		}
+	}
+
+	@Test(expected = OptimisticLockException.class)
+	public void testUpdatingADocumentWithWrongRevisionNumber() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		String firstVersion = createdDocument.getRevision();
+
+		createdDocument = dataStore.saveDocument( createdDocument );
+		createdDocument.setRevision( firstVersion );
+
+		dataStore.saveDocument( createdDocument );
+	}
+
+	@Test(expected = HibernateException.class)
+	public void testCreatingADocumentAfterDeletingDatabase() {
+		dropDatabase();
+
+		dataStore.saveDocument( createEntity() );
+	}
+
+	@Test(expected = HibernateException.class)
+	public void testCreatingInstanceWithWrongUrlUpdatingADocumentUsingAWrongDatabaseUrlConfiguration() throws Exception {
+		dataStore = null;
+		CouchDBDatastore.newInstance( getWrongDatabaseURL(), getUserName(), getPassword(), true );
+	}
+
+	@Test
+	public void testDeleteADocument() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		String createdDocumentId = createdDocument.getId();
+		dataStore.deleteDocument( createdDocument );
+
+		CouchDBEntity entity = dataStore.getEntity( createdDocumentId );
+
+		assertThat( entity, nullValue() );
+	}
+
+	@Test(expected = OptimisticLockException.class)
+	public void testDeleteADocumentWithWrongRevision() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		final String revisionBeforeUpdate = createdDocument.getRevision();
+
+		// saving the document will change its revision value
+		dataStore.saveDocument( createdDocument );
+		createdDocument.setRevision( revisionBeforeUpdate );
+
+		dataStore.deleteDocument( createdDocument );
+	}
+
+	@Test(expected = HibernateException.class)
+	public void testDeleteADocumentNotSavedOnTheDatabase() {
+		CouchDBDocument document = createEntity();
+		dataStore.deleteDocument( document );
+	}
+
+	@Test
+	public void testGetEntity() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		CouchDBEntity entity = dataStore.getEntity( createdDocument.getId() );
+		assertThat( entity, notNullValue() );
+	}
+
+	@Test
+	public void testGetEntityWithWoringIdReturnNullValue() {
+		CouchDBDocument createdDocument = dataStore.saveDocument( createEntity() );
+		CouchDBEntity entity = dataStore.getEntity( createdDocument.getId() + "_1" );
+		assertThat( entity, nullValue() );
+	}
+
+	private void setUpDatastore() throws IOException {
+		dataBaseDropped = false;
+		loadProperties();
+		dataStore = CouchDBDatastore.newInstance( getDatabaseURL(), getUserName(), getPassword(), true );
+	}
+
+	private DataBaseURL getDatabaseURL() throws MalformedURLException {
+		return new DataBaseURL( getDatabaseHost(), getDatabasePort(), getDatabaseName() );
+	}
+
+	private void loadProperties() throws IOException {
+		properties = new Properties();
+		properties.load( CouchDBDialectTest.class.getClassLoader().getResourceAsStream( "hibernate.properties" ) );
+	}
+
+	private String getPassword() {
+		return properties.getProperty( Environment.COUCHDB_PASSWORD );
+	}
+
+	private String getUserName() {
+		return properties.getProperty( Environment.COUCHDB_USERNAME );
+	}
+
+	private int getDatabasePort() {
+		final String port = properties.getProperty( Environment.COUCHDB_PORT );
+		if ( isValueProvided( port ) ) {
+			return Integer.valueOf( port );
+		}
+		else {
+			return DEFAULT_PORT;
+		}
+	}
+
+	private String getDatabaseName() {
+		return properties.getProperty( Environment.COUCHDB_DATABASE );
+	}
+
+	private String getDatabaseHost() {
+		final String host = properties.getProperty( Environment.COUCHDB_HOST );
+		if ( isValueProvided( host ) ) {
+			return host;
+		}
+		else {
+			return LOCALHOST;
+		}
+	}
+
+	private CouchDBEntity createEntity() {
+		return new CouchDBEntity( createEntityKey( "tableName", new String[] { "id", "name" }, new String[] { "1",
+				"Andrea" } ) );
+	}
+
+	private EntityKey createEntityKey(String tableName, String[] columnNames, Object[] values) {
+		return new EntityKey( new EntityKeyMetadata( tableName, columnNames ), values );
+	}
+
+	private DataBaseURL getWrongDatabaseURL() throws MalformedURLException {
+		return new DataBaseURL( "localhost", 1234, "no_existing" );
+	}
+
+	private boolean isValueProvided(String property) {
+		return property != null && !property.trim().equals( "" );
+	}
+
+}

src/test/java/org/hibernate/ogm/dialect/couchdb/util/DataBaseURLTest.java
@@ -1,0 +1,57 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.dialect.couchdb.util;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrea Boriero <dreborier@gmail.com/>
+ */
+public class DataBaseURLTest {
+
+	@Test
+	public void shouldReturnTheCorrectServerURL() throws Exception {
+		String expectedServerURL = "http://localhost:5984";
+		DataBaseURL dataBaseURL = new DataBaseURL( "localhost", 5984, "databasename" );
+
+		assertThat( dataBaseURL.getServerUrl(), is( expectedServerURL ) );
+	}
+
+	@Test
+	public void shouldReturnTheCorrectServerName() throws Exception {
+		String expectedName = "not_important";
+		DataBaseURL dataBaseURL = new DataBaseURL( "localhost", 5984, expectedName );
+
+		assertThat( dataBaseURL.getDataBaseName(), is( expectedName ) );
+	}
+
+	@Test
+	public void shouldReturnTheCorectURLStringRepresentation() throws Exception {
+		String expectedString = "http://localhost:5984/databaseName";
+		DataBaseURL dataBaseURL = new DataBaseURL( "localhost", 5984, "databaseName" );
+
+		assertThat( dataBaseURL.toString(), is( expectedString ) );
+	}
+
+}